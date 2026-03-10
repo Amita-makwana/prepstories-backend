31bf0d17c92465a0b66306816ecfc68180ebdf32
amita makwana <amitamakwana08@gmail.com>
2026-03-10 22:56:55 +0530

    vercel backend setup

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ connectDB();
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`🚀 PrepStories API running on port ${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`🚀 PrepStories API running on port ${PORT}`);
+// });
+
+export default app;
