f5fa082ce621137930808dd0d3c9f78b13675711
016_AmitaMakwana <111377930+Amita-makwana@users.noreply.github.com>
2026-03-11 13:03:00 +0530

    Refactor CORS options to static origin array
    Updated CORS options to use a static array for allowed origins.

--- src/config/corsOptions.js
+++ src/config/corsOptions.js
@@ -1,19 +1,9 @@
 const corsOptions = {
-  origin: (origin, callback) => {
-    const allowed = [
-      process.env.CLIENT_URL,
-      "http://localhost:5173",
-      "http://localhost:5174",
-      "http://127.0.0.1:5173",
-      "http://127.0.0.1:5174"
-    ].filter(Boolean);
-
-    if (!origin || allowed.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
+  origin: [
+    "https://prepstories.vercel.app",
+    "http://localhost:5173",
+    "http://localhost:5174"
+  ],
   credentials: true
 };
 
